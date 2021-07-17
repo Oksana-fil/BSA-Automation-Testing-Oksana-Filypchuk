4eb2c6e2d59b688de20806bc0f3786972b878740
Oksana-fil <83494389+Oksana-fil@users.noreply.github.com>
2021-07-17 13:20:57 +0300

    test with invalide data

---
+++ specs/logining.spec.js
@@ -0,0 +1,45 @@
+const assert = require('assert');
+
+const { expect } = require('chai');
+
+//Go to wesite (url)
+//Enter valide data in boxes
+//Click on "Sing in" button
+//Compare result
+
+describe('Sing-in:', function () {
+
+    it('should be able to sing in', async function () {
+
+        await browser.setWindowSize(1440, 960);
+        await browser.url('http://46.101.234.121/sign-in');
+
+        const emailField = await $('input[name="email"]');
+        const passwordField = await $('input[name="password"]');
+
+        const signInButton = await $('button');
+
+        await emailField.waitForDisplayed({ timeout: 5000 });
+        await emailField.setValue('john_admin1@admin.com');
+
+        await passwordField.waitForDisplayed({ timeout: 5000 });
+        await passwordField.setValue('Pa55word');
+
+        await signInButton.waitForDisplayed({ timeout: 5000 });
+        await signInButton.click();
+
+        await browser.waitUntil(
+            async function () {
+                const url = await browser.getUrl();
+                return url === 'http://46.101.234.121/doctors';
+            },
+            { timeout: 5000 },
+        );
+
+        const url = await browser.getUrl();
+        expect(url).to.be.eql('http://46.101.234.121/doctors')
+
+        await browser.reloadSession();
+    });
+});
+
---
+++ specs/logining_with_invalid_datas.spec.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+
+const { expect } = require('chai');
+
+//Go to wesite (url)
+//Enter invalide data in boxes
+//Click on "Sing in" button
+//Compare result
+
+describe('Sign in:', function() {
+    it('Should error on a invalid data', async function () {
+
+        await browser.setWindowSize(1440, 960);
+        await browser.url('http://46.101.234.121/sign-in');
+
+        const emailField = await $('input[name="email"]');
+        const passwordField = await $('input[name="password"]');
+
+        const signInButton = await $('button');
+    
+        await emailField.waitForDisplayed({ timeout: 5000 });
+        await emailField.setValue('john_admin@admin.com');
+
+        await passwordField.waitForDisplayed({timeout: 5000});
+        await passwordField.setValue('Pa55word');
+
+        await signInButton.waitForDisplayed({ timeout: 5000 });
+        await signInButton.click();
+
+        await browser.waitUntil(
+            async function () {
+              const url = await browser.getUrl();
+              return url === 'http://46.101.234.121/doctors';
+            },
+            { timeout: 5000 },
+          );
+
+        const url = await browser.getUrl();
+        expect(url).to.be.false
+
+        await browser.reloadSession();
+    });
+});
